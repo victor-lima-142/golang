58cdc9308bcd432623032edf242623f394c6eecc
victor-lima-142 <victoreboredo.dev@gmail.com>
2025-01-12 09:14:25 -0300

    Optimizing code with sql

--- fifth-steps/webapp/main.go
+++ fifth-steps/webapp/main.go
@@ -1,17 +1,54 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
 )
 
-var templ = template.Must(template.ParseGlob("templates/*.html"))
-
-func index(w http.ResponseWriter, r *http.Request) {
-	templ.ExecuteTemplate(w, "index.html", nil)
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Quantity    int
 }
 
+func conectDB() (*sql.DB, error) {
+	conectionUrl := "user=postgres dbname=goproducts password=123456 hostname=localhost sslmode=disable"
+
+	db, err := sql.Open("postgres", conectionUrl)
+	return db, err
+}
+
+var templ = template.Must(template.ParseGlob("templates/*.html"))
+
 func main() {
+	db, _ := conectDB()
+	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	products := []Product{
+		{
+			Name:        "Laptop",
+			Description: "Asus",
+			Price:       1000,
+			Quantity:    10,
+		},
+		{
+			Name:        "Mobile",
+			Description: "Samsung",
+			Price:       500,
+			Quantity:    20,
+		},
+		{
+			Name:        "Tablet",
+			Description: "Apple",
+			Price:       800,
+			Quantity:    15,
+		},
+	}
+	templ.ExecuteTemplate(w, "Index", products)
 }
